30e1876f5498e4cda16dc59574a41ae7adb6af4a
David Jimenez <david.jimenez1@hotmail.com>
2024-02-04 04:32:56 -0500

    Create appbar that will help with routing

---
+++ src/components/CustomAppBar.tsx
@@ -0,0 +1,131 @@
+import { useState, ChangeEvent, MouseEvent } from "react";
+
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+  IconButton,
+  Switch,
+  FormControlLabel,
+  FormGroup,
+  MenuItem,
+  Menu,
+  Drawer,
+} from "@mui/material";
+
+import { Menu as MenuIcon, AccountCircle } from "@mui/icons-material";
+import DrawerComponent from "./DrawerComponent";
+
+interface CustomAppBarProps {
+  pageTitle: string;
+}
+
+const CustomAppBar = ({ pageTitle }: CustomAppBarProps) => {
+  const [auth, setAuth] = useState<boolean>(true);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [mobileOpen, setMobileOpen] = useState<boolean>(false);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = (event: MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <FormGroup>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={auth}
+              onChange={handleChange}
+              aria-label="login switch"
+            />
+          }
+          label={auth ? "Logout" : "Login"}
+        />
+      </FormGroup>
+      <AppBar position="fixed">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ display: { sm: "block", md: "none" } }} // Adjust visibility based on screen size
+            onClick={handleDrawerToggle}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            {pageTitle}
+          </Typography>
+          {auth && (
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+              </Menu>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+      <>
+        <Drawer
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{ keepMounted: true }} // Makes it better for mobile.
+          sx={{
+            display: { xs: "block", md: "none" },
+            "& .MuiDrawer-paper": { boxSizing: "border-box", width: 240 },
+          }}
+        >
+          <DrawerComponent
+            pages={[
+              { name: "Home", href: "home" },
+              { name: "About Us", href: "about" },
+            ]}
+            handleDrawerToggle={handleDrawerToggle}
+          />
+        </Drawer>
+      </>
+    </Box>
+  );
+};
+export default CustomAppBar;
