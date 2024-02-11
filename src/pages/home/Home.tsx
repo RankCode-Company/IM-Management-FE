8eacb6e4bb66e898b7253fd867c8a05e37aaa5f7
David Jimenez <david.jimenez1@hotmail.com>
2024-02-11 04:00:17 -0500

    Change title message for home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,20 +1,7 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../../components/components.css'; // Adjusted for a hypothetical styles folder within src
-import { useTranslation } from 'react-i18next';
+import React from "react";
 
-interface LargeButtonProps {
-  path: string;
-  label: string;
-}
-
-const LargeButton: React.FC<LargeButtonProps> = ({ path, label }) => {
-  return (
-    <Link to={path} className="large-nav-button grid-item">
-      {label}
-    </Link>
-  );
-};
+import { useTranslation } from "react-i18next";
+import LargeButton from "../../components/LargeButton";
 
 const Home: React.FC = () => {
   const { t } = useTranslation();
@@ -26,7 +13,7 @@ const Home: React.FC = () => {
       <LargeButton path="/spreadsheet" label="Template 1" />
       <LargeButton path="/spreadsheet" label="Template 2" />
       <LargeButton path="/orders" label="Template 3" />
-      <span>{t("home.homepage_title")}</span>
+      <span>{t("home.welcome_message")}</span>
     </div>
   );
 };
